da8bc366f780274e12f8a737e56457c25d95ff67
Michael Pingleton <mnpingleton@gmail.com>
2021-08-13 18:37:19 +0000

    Delete request processing working.

--- index.js
+++ index.js
@@ -14,12 +14,20 @@ const database = knex({
 
 
 app.get('/', (req, res) => {
+	console.log('Get request received.');
 	database.select('*').from('movies')
 		.then(data => res.status(200).json(data));
 });
 
 app.get('/movies/:movieId', (req, res) => {
+	console.log('Get request received within route.');
 	database.select('*').from('movies').where({title: req.params.movieId})
+		.then(data => res.status(200).json(data));
+});
+
+app.delete('/movies/:movieId', (req, res) => {
+	console.log('Delete request received.');
+	database.from('movies').where({title: req.params.movieId}).del()
 		.then(data => res.status(200).json(data));
 });
 
